ee0a68c8051e7d259a21af0f6c43e36906c29f79
jesus-molano <jessumolano@gmail.com>
2023-02-18 21:59:30 +0000

    Finished card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
 import Card3D from '@components/Card3D'
 import '@styles/App.css'
 
 function App() {
+  const [coords, setCoords] = useState({ x: 0, y: 0 })
 
+  const handleMouseMove = (event: React.MouseEvent) => {
+    setCoords({ x: event.clientX, y: event.clientY })
+  }
+  const handleTouchMove = (event: React.TouchEvent) => {
+    const touch = event.touches[0]
+    setCoords({ x: touch.clientX, y: touch.clientY })
+  }
   return (
-    <div className="App">
-      <Card3D />
+    <div className="App" onMouseMove={handleMouseMove} onTouchMove={handleTouchMove}>
+      <Card3D coords={coords} />
     </div>
   )
 }
--- src/components/Card3D/index.tsx
+++ src/components/Card3D/index.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useRef } from 'react'
 import './card3d.css'
+type Props = {
+  coords: {
+    x: number,
+    y: number
+  }
+}
+function Card3D({ coords }: Props) {
+  console.log(coords);
 
-function Card3D() {
+  const card = useRef(null)
+  const middleX = window.innerWidth / 2
+  const middleY = window.innerHeight / 2
+  // Degrees (45 == max)
+  const offsetX = ((coords.x - middleX) / middleX) * 45
+  const offsetY = ((coords.y - middleY) / middleY) * 45
 
+  useEffect(() => {
+    if (card.current) {
+      card.current.style.setProperty('--rotateX', -1 * offsetY + "deg")
+      card.current.style.setProperty('--rotateY', offsetX + "deg")
+    }
+  }, [coords])
   return (
-    <div className="card-3d" tabIndex={0}>
+    <div className="card-3d" tabIndex={0} ref={card}>
       <h1>Card</h1>
+      <p>x: {coords.x}</p>
+      <p>y: {coords.y}</p>
     </div>
   )
 }
