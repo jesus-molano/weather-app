2d0897df2e4837836bfb2aea7a584e7ffdf3ad02
jesus-molano <jessumolano@gmail.com>
2023-02-20 23:48:33 +0000

    Adjusted the position of card3D and searchbar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -5,7 +5,6 @@ import { BiSearchAlt } from 'react-icons/bi'
 import './searchbar.css'
 import CustomAlert from '@components/CustomAlert'
 
-
 const SearchBar: React.FC = () => {
   const { setLocation } = useContext(LocationContext)
   const [value, setValue] = useState('')
@@ -16,51 +15,48 @@ const SearchBar: React.FC = () => {
     onOk: () => setError(false)
   }
   let typingTimer: number | undefined = undefined
-  const [typingTimeout, setTypingTimeout] = useState(0);
+  const [typingTimeout, setTypingTimeout] = useState(0)
   const doneTypingInterval = 1000
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('me ejecuto 1');
-    
     const city = e.target.value
     clearTimeout(typingTimer)
     setValue(e.target.value)
     if (typingTimeout) {
-      clearTimeout(typingTimeout);
-      console.log('me ejecuto 2');
-
+      clearTimeout(typingTimeout)
     }
-    
-    setTypingTimeout(setTimeout(async (city: string) => {
-      console.log('me ejecuto 3');
 
-      if(city === '') return
-      const data = await getLatLonCity(city)
-      console.log('me ejecuto 4');
+    setTypingTimeout(
+      setTimeout(
+        async (city: string) => {
+          if (city === '') return
+          const data = await getLatLonCity(city)
 
-      if (data.length === 0) {
-        setValue('')
-        console.log('me ejecuto 5');
-        return setError(true)
-
-      }
-      const lat = data[0].lat
-      const lon = data[0].lon
-      setLocation({ latitude: lat, longitude: lon })
-      setValue('')
-      console.log('me ejecuto 6');
-
-      
-    }, doneTypingInterval, city));
-      
-    
+          if (data.length === 0) {
+            setValue('')
+            return setError(true)
+          }
+          const lat = data[0].lat
+          const lon = data[0].lon
+          setLocation({ latitude: lat, longitude: lon })
+          setValue('')
+        },
+        doneTypingInterval,
+        city
+      )
+    )
   }
   return (
     <>
       {error && <CustomAlert options={options} />}
-      <div className='searchbar' >
-        <input type='text' placeholder='Search a city...' onChange={handleChange} value={value} />
-          <BiSearchAlt className='search-icon' size={30} />
+      <div className='searchbar'>
+        <input
+          type='text'
+          placeholder='Search a city...'
+          onChange={handleChange}
+          value={value}
+        />
+        <BiSearchAlt className='search-icon' size={30} />
       </div>
     </>
   )
